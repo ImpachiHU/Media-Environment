5b36844d3838bec056d7713f6e012c2342051670
ImpachiHU <JNova@my.harrisburgu.edu>
2025-01-23 11:28:08 -0500

    Week 3 HW

---
+++ Week 3/Week03_Text.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(800, 400);
+background(226, 213, 135);
+}
+function draw() {
+  fill(420, 69, 21)
+  textStyle(BOLDITALIC)
+  textFont('Verdana', 32)
+  text('Harrisburg University', 50, 50);
+  fill(0, 0, 0)
+  textWidth(700)
+  textStyle(NORMAL)
+  textFont('Courier New', 14)
+  text('I applied to HU in 2022 to be part of the ESPT program. In the summer of 2024, I switched to IMED. Now I am writing these three sentences as part of my school assignments.', 100, 100, 700)
+}
